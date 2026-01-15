7367d02d08e7fc113a0d9ed001e441a4c0bebee1
Gaurav <gaurav13bisht@gmail.com>
2026-01-15 23:34:05 +0530

    Updated Strategy pattern

--- DesignPatterns/DecoratorDesignPattern/Decorators/PizzaDecorator.java
+++ DesignPatterns/DecoratorDesignPattern/Decorators/PizzaDecorator.java
@@ -2,7 +2,7 @@ package DesignPatterns.DecoratorDesignPattern.Decorators;
 
 import DesignPatterns.DecoratorDesignPattern.BasicPizza;
 
-public interface PizzaDecorator extends BasicPizza {
+public interface PizzaDecorator {
     public int calories();
     public int cost();
 }
