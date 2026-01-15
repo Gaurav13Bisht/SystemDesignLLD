08ab7385b86dde7c8966f770004e151a007f0c68
Gaurav <gaurav13bisht@gmail.com>
2026-01-15 23:04:24 +0530

    Added another Abstract factory design pattern.

---
+++ DesignPatterns/AbstractFactoryDesignPattern/CarExample/CarExterior.java
@@ -0,0 +1,7 @@
+package DesignPatterns.AbstractFactoryDesignPattern.CarExample;
+
+// Step 1: Abstract Product interfaces - Define product families
+public interface CarExterior {
+
+    void addExteriorComponents();
+}
